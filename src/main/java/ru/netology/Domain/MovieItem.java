212787f0bcb0d5190b6377c94ad6dc4476e059a7
Darya-Rak <Darya-Rak@users.noreply.github.com>
2021-04-24 21:13:52 +0300

    Task 1

--- src/main/java/ru/netology/Domain/MovieItem.java
+++ src/main/java/ru/netology/Domain/MovieItem.java
@@ -7,14 +7,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
-    public class MovieItem {
-        private int movieId;
-        private String movieName;
-        private String genre;
+public class MovieItem {
+    private int movieId;
+    private String movieName;
+    private String genre;
 
     public int getMovieId() {
         return movieId;
     }
 }
-
